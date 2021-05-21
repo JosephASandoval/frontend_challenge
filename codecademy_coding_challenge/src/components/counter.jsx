e1c87c0811133b0becdc5ea98c95114b7a1307d7
JosephASandoval <josephasandoval17@gmail.com>
2021-05-21 13:00:49 -0700

    count is now working

--- codecademy_coding_challenge/src/components/counter.jsx
+++ codecademy_coding_challenge/src/components/counter.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
   formatCount() {
-    const { count } = this.state;
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
   }
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary">{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
